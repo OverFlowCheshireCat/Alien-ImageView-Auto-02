221df84b67cc6132b000f64d7f0036c6e159a499
dit03 <dit@imac-4.local>
2019-03-28 16:41:46 +0900

    Update Source

--- Alien ImageView Auto 02/ViewController.swift
+++ Alien ImageView Auto 02/ViewController.swift
@@ -1,20 +1,58 @@
-//
-//  ViewController.swift
-//  Alien ImageView Auto 02
-//
-//  Created by dit03 on 2019. 3. 28..
-//  Copyright © 2019년 201730529 김민철. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var countLbl: UILabel!
+    
+    var check = false, count = 1
+    var mTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "frame1.png")
     }
 
-
+    func autoChange() {
+        if (!check) {
+            if (count == 5) {
+                check = true;
+                count -= 1;
+            } else {
+                count += 1;
+            }
+        } else {
+            if (count == 1) {
+                check = false;
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+        
+        imageView.image = UIImage(named: "frame" + String(count) + ".png")
+        countLbl.text = String(count) + " 번 사진"
+    }
+    
+    @IBAction func playBtn(_ sender: Any) {
+        if (!mTimer.isValid) {
+            mTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
+                self.autoChange()
+            })
+        }
+    }
+    
+    @IBAction func pauseBtn(_ sender: Any) {
+        if (mTimer.isValid) {
+            mTimer.invalidate()
+        }
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        mTimer.invalidate()
+            
+        count = 1
+            
+        imageView.image = UIImage(named: "frame" + String(count) + ".png")
+        countLbl.text = String(count) + " 번 사진"
+    }
 }
-
